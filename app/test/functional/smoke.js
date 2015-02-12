94a5bed54e588d0165116e1c47cfd6da0abe275a
Eric Elliott <support@paralleldrive.com>
2015-02-11 19:08:08 -0700

    Pull in selenium-sauce.

---
+++ app/test/functional/smoke.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var selenium = require('selenium-sauce');
+
+// Loads the config file and invokes the callback once for each browser
+module.exports = new selenium({   // Configuration options
+  quiet: false,           // Silences the console output
+  webdriver: {            // Options for Selenium WebDriver (WebdriverIO)
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    desiredCapabilities: [{
+      browserName: 'chrome',
+      version: '27',
+      platform: 'XP',
+      tags: ['examples'],
+      name: 'This is an example test'
+    }]
+  },
+  sauceLabs: {            // Options for SauceLabs API wrapper (npmjs.org/package/saucelabs)
+    username: process.env.SAUCE_USERNAME,
+    password: process.env.SAUCE_ACCESS_KEY
+  },
+  sauceConnect: {         // Options for SauceLabs Connect (npmjs.org/package/sauce-connect-launcher)
+    username: process.env.SAUCE_USERNAME,
+    accessKey: process.env.SAUCE_ACCESS_KEY
+  },
+  selenium: {             // Options for Selenium Standalone Server (npmjs.org/package/selenium-standalone). Only used if you need Selenium running locally.
+    args: []                // options to pass to `java -jar selenium-server-standalone-X.XX.X.jar`
+  }
+},
+function (browser) {
+  var args = arguments;
+
+  // Initialize the browser
+  browser.init(function(err) {
+    if (err) { throw err; }
+
+    // Load a url into the browser
+    browser.url('http://google.com', function() {
+
+      test.equal({}, browser, 'Browser object dump');
+
+      test.equal({}, arguments, 'arguments dump');
+
+      // Tell SauceLabs that the test was successful
+      browser.report(true, function report() {
+        test.equal({}, arguments, 'report arguments dump');
+      });
+    });
+  });
+});
