88ea004025ce4a9e101cc99a6570428c6fc0b5b8
Eric Elliott <support@paralleldrive.com>
2015-11-10 20:11:21 -0700

    Share main route with devServer.

--- source/server/index.js
+++ source/server/index.js
@@ -1,34 +1,19 @@
 import path from 'path';
 import express from 'express';
-import settings from './settings';
+import settings from 'server/settings';
 
-import render from './render.js';
-import renderLayout from './render-layout.js';
+import mainRoute from './routes/main';
 
 const app = express();
 
 const port = process.env.APP_PORT || 3000;
-const title = 'Your App';
-const props = {
-  title: 'Your App'
-};
+
 const buildDir = '/build';
 const staticDir = path.join(settings.APP_HOME, buildDir);
 
-const rootMarkup = render(props);
-
-// This will be rendered into the HTML to pass data to the client.
-const payload = `
-  var payload = {
-    title: '${ props.title }'
-  };
-`;
-
 app.use('/static', express.static(staticDir));
-
-app.get('*', (req, res) => {
-  res.send(renderLayout({ title, rootMarkup, payload}));
-});
+console.log(mainRoute);
+app.use('*', mainRoute);
 
 app.listen(port, 'localhost', (err) => {
   if (err) {
