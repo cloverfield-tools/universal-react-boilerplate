11b1e2fa7b50f290ad238fc55b30e54c7c29bee7
Guido Schmitz <guisch95@gmail.com>
2016-01-13 06:43:09 +0100

    relative to absolute path

--- source/client/index.js
+++ source/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import createApp from '../shared/components/App';
+import createApp from 'shared/components/App';
 import { Provider } from 'react-redux';
 import configureStore from 'shared/configureStore';
 
