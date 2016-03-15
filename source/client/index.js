23cee4a3572a4d7541ff85a132f28e938b715024
satbir1 <satbir.s@homelane.com>
2016-03-15 23:21:07 +0530

    Updated react-router-redux related code.

--- source/client/index.js
+++ source/client/index.js
@@ -2,35 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { browserHistory } from 'react-router';
-import { syncHistory, routeReducer } from 'react-router-redux';
+import { Router, Route, browserHistory  } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import reducers from 'shared/reducers';
 
-import { Router, Route } from 'react-router';
 import createApp from 'shared/components/app';
 import createTestData from 'shared/components/test-data';
 
 
-const reducer = combineReducers(Object.assign({}, reducers, {
-  routing: routeReducer
-}));
 
-// Sync dispatched route actions to the history
-const reduxRouterMiddleware = syncHistory(browserHistory);
-const createStoreWithMiddleware =
-  applyMiddleware(reduxRouterMiddleware)(createStore);
 
-const store = createStoreWithMiddleware(reducer);
+
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+      ...reducers,
+    routing: routerReducer
+})
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
 
 // Required for replaying actions from devtools to work
 // reduxRouterMiddleware.listenForReplays(store)
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={ browserHistory }>
+    <Router history={ history }>
       <Route path="/" component={ createApp(React) } />
       <Route path="/test-data" component={ createTestData(React) } />
     </Router>
   </Provider>,
   document.getElementById('root')
 );
+
+
+
