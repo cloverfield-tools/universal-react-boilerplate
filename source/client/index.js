65ef1243f34115676b7dfc3e8545bf954a761c2c
satbir1 <satbir.s@homelane.com>
2016-03-15 23:45:53 +0530

    Fixing build failing due to linting issues.

--- source/client/index.js
+++ source/client/index.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory  } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import reducers from 'shared/reducers';
 
 import createApp from 'shared/components/app';
 import createTestData from 'shared/components/test-data';
 
-
-
-
-
 // Add the reducer to your store on the `routing` key
 const store = createStore(
   combineReducers({
-      ...reducers,
+    ...reducers,
     routing: routerReducer
-})
-)
+  })
+);
 
 // Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(browserHistory, store);
 
 
 // Required for replaying actions from devtools to work
@@ -37,6 +33,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-
-
